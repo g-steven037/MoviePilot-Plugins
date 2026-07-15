9760985c57dfdce39a09a4615bb2f3582e139f3b
g-steven037 <102415428+g-steven037@users.noreply.github.com>
2026-07-15 15:44:48 +0800

    Brighten Emby plugin card and release v0.1.2

--- plugins.v2/embylibrarycover/__init__.py
+++ plugins.v2/embylibrarycover/__init__.py
@@ -24,8 +24,8 @@ DEFAULT_LIBRARY_MAP = "电影|电影|MOVIES\n剧集|剧集|TV SERIES\n动画|动
 class EmbyLibraryCover(_PluginBase):
     plugin_name = "Emby媒体库封面"
     plugin_desc = "根据Emby最新媒体海报生成横版媒体库封面，可按Cron定时生成并选择性上传覆盖，仅自用测试。"
-    plugin_icon = "https://raw.githubusercontent.com/jxxghp/MoviePilot-Frontend/v2/src/assets/images/misc/emby.png"
-    plugin_version = "0.1.1"
+    plugin_icon = "https://raw.githubusercontent.com/g-steven037/MoviePilot-Plugins/main/assets/emby-library-cover.svg"
+    plugin_version = "0.1.2"
     plugin_author = "g-steven037"
     author_url = "https://github.com/g-steven037"
     plugin_config_prefix = "embylibrarycover_"
